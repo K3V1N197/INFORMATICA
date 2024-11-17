07f6ddf4647197ddcfbdde870de9560d992dbab1
kev <89529852+K3V1N197@users.noreply.github.com>
2024-11-17 17:30:30 +0000

    update

--- C/10 (file di record)/esercizioA.c
+++ C/10 (file di record)/esercizioA.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #define NUM_STUDENTI 10
 
 typedef struct {
-    char cognome[50];
+    char cognome[25];
     int voti[3];
 } Studente;
 
-void salvaSuFile();
-void caricaDaFile();
-int trovaCognome(char cogn[]);
-void stampaDettagli();
+void salvaSuFile(char NomeFile[]);
+void caricaDaFile(char NomeFile[]);
+int trovaCognome(char NomeFile[], char cognomeInserito[]);
+void stampaDettagli(char NomeFile[]);
 
 int main() {
-    char cognomeInserito[50];
+    char cognomeInserito[25];
+    char NomeFile[] = "studenti.dat";
 
-    salvaSuFile();
-    caricaDaFile();
+    salvaSuFile(NomeFile);
+    caricaDaFile(NomeFile);
 
     printf("\nInserisci un cognome da cercare: ");
     scanf("%s", cognomeInserito);
-    int conteggio = trovaCognome(cognomeInserito);
+    int conteggio = trovaCognome(NomeFile, cognomeInserito);
     
     if (conteggio > 0) {
         printf("Il cognome %s è stato trovato %d volte\n", cognomeInserito, conteggio);
@@ -28,101 +30,106 @@ int main() {
         printf("Il cognome %s non è stato trovato\n", cognomeInserito);
     }
 
-    stampaDettagli();
+    stampaDettagli(NomeFile);
 
     return 0;
 }
 
-void salvaSuFile() {
-    FILE *file = fopen("studenti.dat", "wb");
-    Studente studenti[NUM_STUDENTI];
-
+void salvaSuFile(char NomeFile[]) {
+    FILE *file = fopen(NomeFile, "wb");
     if (file == NULL) {
         perror("Errore durante l'apertura del file");
         return;
     }
 
+    Studente studenti[NUM_STUDENTI];
+
     for (int i = 0; i < NUM_STUDENTI; i++) {
-        printf("Inserisci il cognome dello studente: ");
+        printf("Inserisci il cognome dello studente %d: ", i + 1);
         scanf("%s", studenti[i].cognome);
         
         for (int j = 0; j < 3; j++) {
             printf("Inserisci il voto #%d: ", j + 1);
             scanf("%d", &studenti[i].voti[j]);
         }
-        
+
         fwrite(&studenti[i], sizeof(Studente), 1, file);
     }
+
     fclose(file);
 }
 
-void caricaDaFile() {
-    FILE *file = fopen("studenti.dat", "rb");
-    Studente studente;
-
+void caricaDaFile(char NomeFile[]) {
+    FILE *file = fopen(NomeFile, "rb");
     if (file == NULL) {
         perror("Errore durante l'apertura del file");
         return;
     }
 
-    while (fread(&studente, sizeof(studente), 1, file) == 1) {
-        printf("Cognome: %s\n", studente.cognome);
+    Studente buffer;
+
+    while (fread(&buffer, sizeof(Studente), 1, file) == 1) {
+        printf("Cognome: %s\n", buffer.cognome);
         
         printf("Voti: ");
         for (int i = 0; i < 3; i++) {
-            printf("%d ", studente.voti[i]);
+            printf("%d ", buffer.voti[i]);
         }
         printf("\n");
     }
+
     fclose(file);
 }
 
-int trovaCognome(char cogn[]) {
-    FILE *file = fopen("studenti.dat", "rb");
+int trovaCognome(char NomeFile[], char cogn[]) {
+    FILE *file = fopen(NomeFile, "rb");
     if (file == NULL) {
         perror("Errore durante l'apertura del file");
         return 0;
     }
-    
-    Studente studente;
+
+    Studente buffer;
     int conteggio = 0;
 
-    while (fread(&studente, sizeof(Studente), 1, file) == 1) {
-        if (strcmp(studente.cognome, cogn) == 0) {
+    while (fread(&buffer, sizeof(Studente), 1, file) == 1) {
+        if (strcmp(buffer.cognome, cogn) == 0) {
             conteggio++;
         }
     }
+
     fclose(file);
     return conteggio;
 }
 
-void stampaDettagli() {
-    FILE *file = fopen("studenti.dat", "rb");
-    
+void stampaDettagli(char NomeFile[]) {
+    FILE *file = fopen(NomeFile, "rb");
     if (file == NULL) {
         perror("Errore durante l'apertura del file");
         return;
     }
-    
-    Studente studente;
 
-    while (fread(&studente, sizeof(Studente), 1, file) == 1) {
-        int maxVoto = 0, minVoto = 10, sommaVoti = 0;
-        
+    Studente buffer;
+
+    while (fread(&buffer, sizeof(Studente), 1, file) == 1) {
+        int maxVoto = buffer.voti[0];
+        int minVoto = buffer.voti[0];
+        int sommaVoti = 0;
+
         for (int i = 0; i < 3; i++) {
-            sommaVoti += studente.voti[i];
-            if (studente.voti[i] > maxVoto) {
-                maxVoto = studente.voti[i];
+            sommaVoti += buffer.voti[i];
+            if (buffer.voti[i] > maxVoto) {
+                maxVoto = buffer.voti[i];
             }
-            if (studente.voti[i] < minVoto) {
-                minVoto = studente.voti[i];
+            if (buffer.voti[i] < minVoto) {
+                minVoto = buffer.voti[i];
             }
         }
-        
-        float mediaVoti = (float) sommaVoti / 3;
-        
-        printf("Cognome: %s, Media: %.2f, Voto massimo: %d, Voto minimo: %d\n", studente.cognome, mediaVoti, maxVoto, minVoto);
-        printf("\n");
+
+        float mediaVoti = (float)sommaVoti / 3;
+
+        printf("Cognome: %-25s, Media: %.2f, Voto massimo: %d, Voto minimo: %d\n",
+               buffer.cognome, mediaVoti, maxVoto, minVoto);
     }
+
     fclose(file);
 }
