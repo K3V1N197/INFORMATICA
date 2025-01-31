08751e2cf08e39b89bec4afac2938e9739699867
kev <89529852+K3V1N197@users.noreply.github.com>
2025-01-31 07:51:13 +0000

    done

---
+++ C++/01 (classi)/progetto frazione/Frazione.h
@@ -0,0 +1,16 @@
+#ifndef FRAZIONE_H
+#define FRAZIONE_H
+
+class Frazione
+{
+private:
+  int numeratore;
+  int denominatore;
+
+public:
+  Frazione(int numeratore, int denominatore);
+
+  void Stampa();
+  void Semplifica();
+};
+#endif
